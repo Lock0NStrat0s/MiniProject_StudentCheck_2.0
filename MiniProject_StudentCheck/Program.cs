336a833ed4cd05490939df075501b6fb887927ef
Mohammad Jabir <mohammadj94@gmail.com>
2025-03-07 16:29:47 -0700

    initial commit

---
+++ MiniProject_StudentCheck/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniProject_StudentCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
